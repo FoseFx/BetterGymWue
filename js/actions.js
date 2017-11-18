880c4568c6f2aadcd43433bda122625448f5dc7f
FoseFx <info@fosefx.de>
2017-11-18 16:24:06 +0100

    terms fixes

--- js/actions.js
+++ js/actions.js
@@ -2,6 +2,7 @@ $("#termsaccept").on("click", function (e) {
     e.preventDefault();
     document.cookie = "a=true;expires=Fri, 31 Dec 9999 23:59:59 GMT";
     $("#terms").css("display", "none");
+    moveInLogin();
 });
 
 $("#login-form").submit(function (e) {
