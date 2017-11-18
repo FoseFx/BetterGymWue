fab0385f90ce441a18269eb0e858b37f8a0ea786
FoseFx <info@fosefx.de>
2017-11-18 19:09:23 +0100

    bunch of fixes

--- js/actions.js
+++ js/actions.js
@@ -7,28 +7,16 @@ $("#termsaccept").on("click", function (e) {
 
 $("#login-form").submit(function (e) {
     e.preventDefault();
-
+    var key = btoa($("#select").val() + ":" + $("#psw").val());
+    console.log(key);
     $.ajax({
         type: "GET",
-        headers:{"Authorization": "Basic " + btoa($("#select").val() + ":" + $("#psw").val())},
-        url: 'https://www.fosefx.de/betterGymWue/mirror.php?url=https://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/45/c/c00020.htm',
-        crossDomain: true,
-        statusCode: {
-            401: function (r) {
-                unAuth();
-            },
-            304: function (r) {
-                onTrue();
-            },
-            500: function (r) {
-                console.log("500");
-            },
-            301: function (r) {
-                console.log("301");
-            }
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", "Basic " + key);
         },
+        url: 'https://www.fosefx.de/betterGymWue/mirror.php?url=gymnasium-wuerselen.de/untis/Schueler-Stundenplan/45/c/c00020.htm',
         success: function (r) {
-            onTrue();
+            onTrue(key);
         },
         error: function (e, r, t) {
             console.log(e.getAllResponseHeaders());
@@ -38,8 +26,7 @@ $("#login-form").submit(function (e) {
 
 });
 
-function onTrue() {
-    document.cookie = "user=" + $("#select").val() + ";expires=Fri, 31 Dec 9999 23:59:59 GMT";
-    document.cookie = "psw=" + $("#psw").val() + ";expires=Fri, 31 Dec 9999 23:59:59 GMT";
+function onTrue(key) {
+    document.cookie = "key=" + key + ";expires=Fri, 31 Dec 9999 23:59:59 GMT";
     Auth();
 }
