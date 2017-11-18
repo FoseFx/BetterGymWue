5e95518004280a8abddc422a7cde59689279bb81
FoseFx <info@fosefx.de>
2017-11-18 19:51:38 +0100

    Login funktioniert

--- js/util.js
+++ js/util.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     document.cookie.split(";").forEach(function (t) {if(t == "a=true") a = true;});
     stopSpinner();
     if(!a) showAGB();
-    if(a) moveInLogin();
+    if(a) checkLoginCookie();
 });
 
 function stopSpinner() {
@@ -32,6 +32,13 @@ function showAGB() {
     $("#terms").css("display", "flex");
 }
 
+function checkLoginCookie() {
+    var c = null;
+    document.cookie.split(";").forEach(function (t) {var l = t.split("="); if(l[0].replace(/\s/g, '') === "key") c = l[1]});
+    if(c !== null)
+        login(c, 1);
+    else moveInLogin();
+}
 function moveInLogin() {
     setTimeout(function () {
         $("#login-wrapper").css("transform", "translate(-50%,-50%)");
@@ -42,9 +49,14 @@ function unAuth() {
         .addClass("shake shake-constant");
     setTimeout(function () {
         $("#login-wrapper").removeClass("shake shake-constant");
-    }, 100);
+        $("#psw").val("");
+    }, 200);
 }
-function Auth() {
+function Auth(key) {
+    if(key){
+        document.cookie = "key=" + key + ";expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    }
+
     $("#login-wrapper").css("opacity", "0");
     setTimeout(function () {
         $("#login-wrapper").css("display", "none");
