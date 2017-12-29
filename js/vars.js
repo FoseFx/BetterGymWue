eca1feef6bee869fbcb617dec655f59c3f3bda3d
FoseFx <info@fosefx.de>
2017-12-29 17:54:19 +0100

    Multiple Server Support

--- js/vars.js
+++ js/vars.js
@@ -12,3 +12,24 @@ var Gkey;
 var GKURSE = [{kurse: []},{kurse: []}];
 var GTimeTable = [];
 var GMyKurse = [];
+
+
+
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
