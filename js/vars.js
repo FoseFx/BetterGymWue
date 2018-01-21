b2e01e2a11a0f22eb042290b883e95c46753891e
FoseFx <info@fosefx.de>
2018-01-21 17:48:19 +0100

    Minor Changes

--- js/vars.js
+++ js/vars.js
@@ -1,10 +1,10 @@
 const EXP = ";expires=Fri, 31 Dec 9999 23:59:59 GMT";
-// */untis/*
+// Path: */untis/*
 var GURLS = [
     "https://www.fosefx.de/betterGymWue/mirror.php?url=http://gymnasium-wuerselen.de/untis/"
 ];
 var GURL = GURLS[Math.floor(Math.random() * (GURLS.length - 1))];
-
+var GKursAPIURL = "https://www.fosefx.com/betterGymWue/kursAPI.php";
 var Gstufe;
 var GStufeid;
 var GStufeA;
