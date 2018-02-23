fbca630822fc9df2c33d5768f70a512334404523
FoseFx <info@fosefx.de>
2018-02-23 18:44:05 +0100

    Restructure

--- v1-x/src/app/app.module.ts
+++ v1-x/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+import { AppComponent } from './c/A_root/app.component';
+import {routes} from './routes';
+import { AgbComponent } from './c/agb/agb.component';
 
-import { AppComponent } from './app.component';
-
+import * as bootstrap from 'bootstrap';
+import {BaseService} from './s/base.service';
+import { LoginComponent } from './c/login/login.component';
+import {GuardService} from './s/guard.service';
+import {HttpClientModule} from '@angular/common/http';
+import { SelectComponent } from './c/select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgbComponent,
+    LoginComponent,
+    SelectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BaseService, GuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
