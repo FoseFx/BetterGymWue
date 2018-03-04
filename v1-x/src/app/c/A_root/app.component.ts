71521bef8909f7ecc707830bf5cce7aca52c09ec
FoseFx <info@fosefx.de>
2018-03-04 15:46:17 +0100

    Swipe support

--- v1-x/src/app/c/A_root/app.component.ts
+++ v1-x/src/app/c/A_root/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {BaseService} from '../../s/base.service';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
 
   constructor(public baseService: BaseService){};
+
+
+  @ViewChild('hamNav') hamnav;
+  @ViewChild('cntnd') content;
+
+  ngAfterViewInit(){
+    console.log(this.hamnav);
+  }
+
+  swipe(type, e){
+    if ($(".fuckYou").has(e.target).length != 0) return;
+    if(type === 'r' && this.hamnav.opened === false){
+      this.hamnav.open();
+    }if(type === 'l'){
+      this.hamnav.close();
+    }
+  }
+
 
 }
--- v1-x/src/app/c/show/show.component.ts
+++ v1-x/src/app/c/show/show.component.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewChildren} from '@angular/core';
 import {BaseService} from '../../s/base.service';
 import {AlertService} from '../../s/alert.service';
 import * as $ from 'jquery';
+import {MatTab, MatTabGroup} from '@angular/material';
 
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
-export class ShowComponent implements OnInit {
+export class ShowComponent implements OnInit, AfterViewInit {
 
-  selI = 0;
   tts: {
     tag,
     date: Date
   }[] = [];
+
+  @ViewChild(MatTabGroup) group;
+  @ViewChildren(MatTab) tabs;
+  tab_num = 0;
+  selected = 0;
+  SWIPE_ACTION = {
+    LEFT: 'swipeleft',
+    RIGHT: 'swiperight'
+  };
+
+  ngAfterViewInit(){
+    this.baseService.noswipe = true;
+    this.tab_num = this.tabs.length;
+    console.log(this.group);
+    if(!localStorage.firstVisit){
+      localStorage.firstVisit = "false";
+      location.reload();
+    }
+  }
+  swipe(e){
+    let eType = e.type;
+    if (eType === this.SWIPE_ACTION.RIGHT && this.selected > 0){
+      this.selected--;
+    }else if (eType === this.SWIPE_ACTION.LEFT && this.selected < this.tab_num -1 ){
+      this.selected++;
+    }
+  }
 
   constructor(private baseService: BaseService, private alert: AlertService) { }
 
@@ -22,8 +49,11 @@ export class ShowComponent implements OnInit {
     let tt = this.baseService.TT;
     if (!tt) { this.alert.alert("Kein Stundenplan gesetzt", this.alert.DANGER); return; }
 
+    let weeksTT = tt[(getWeekNumber(new Date()) % 2 == 0) ? 0: 1];
+    console.log(weeksTT);
     let firstDate;
-    tt.forEach((val, i) => {
+
+    [0, 1].forEach((val, i) => {
       let date = new Date();
 
       if (i === 1) date.setDate(date.getDate() + 1);
@@ -32,9 +62,25 @@ export class ShowComponent implements OnInit {
       if (i == 0) firstDate = date;
       if(i === 1 && firstDate.getDate() == date.getDate())
         date.setDate(date.getDate() + 1);
-      this.tts.push({tag: val.days[date.getDay() - 1], date: date});
+      this.tts.push({tag: weeksTT.days[date.getDay() - 1], date: date});
     });
     $(".modal-backdrop").remove();
+
   }
 
+
+
+}
+function getWeekNumber(d) {
+  // Copy date so don't modify original
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  // Set to nearest Thursday: current date + 4 - current day number
+  // Make Sunday's day number 7
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+  // Get first day of year
+  let yearStart:any = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+  // Calculate full weeks to nearest Thursday
+  let weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+  // Return array of year and week number
+  return weekNo;
 }
