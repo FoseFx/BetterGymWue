747c365925393c537e51d3eac82361be221f79f6
FoseFx <info@fosefx.de>
2018-03-18 16:09:57 +0100

    Improved Offline Support, Beta

--- v1-x/src/app/c/error/error.component.ts
+++ v1-x/src/app/c/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-error',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(public route: ActivatedRoute) { }
 
+  get lenght():number{
+    return Object.keys(this.route.snapshot.queryParams).length
+  }
   ngOnInit() {
+    console.log(this.route.snapshot.queryParams);
   }
 
 }
