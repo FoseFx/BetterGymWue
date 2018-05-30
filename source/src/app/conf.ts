2fc2093d01b01921fe81c39c43c5de73ed940307
FoseFx <info@fosefx.de>
2018-05-30 20:23:57 +0200

    1.1.9 Beta > 1.2.0 Beta - db URL rewire, Lehrer switch offline not visible when not needed, Scrollbar invisible, gtag bgw_version_in_use metric added

--- source/src/app/conf.ts
+++ source/src/app/conf.ts
@@ -1,10 +1,12 @@
 const baseUrl = 'https://www.fosefx.de/betterGymWue/mirror.php?url=http://gymnasium-wuerselen.de/untis/';
+const dd = "https://bettergymwue.firebaseio.com/";
 export const CONFIG = {
   baseURL: baseUrl,
   credentialsCheckUrl: baseUrl + 'Schueler-Stundenplan/frames/navbar.htm', // also get Stufen
   credentialsCheckLehrerUrl: baseUrl + 'Lehrer-Stundenplan/frames/navbar.htm',
   baseKursURL: baseUrl + 'Schueler-Stundenplan/', // baseKursURL + WOCHE + /c/c + STUFENDIFF
-  dbUrl: 'https://bettergymwue.firebaseio.com/kurse/',
+  dbUrl: dd + 'kurse/',
   vertURL: baseUrl + 'Schueler/',
-  lehrerURL: baseUrl + 'Lehrer/' // .. f1/subst_001.htm
+  lehrerURL: baseUrl + 'Lehrer/', // .. f1/subst_001.htm
+  databaseURL: dd
 };
