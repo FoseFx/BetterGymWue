7bc189184a6f495a3bc139c232070d4057c068e9
FoseFx <info@fosefx.de>
2018-05-31 16:02:59 +0200

    1.1.9 Beta > 1.2.0 Beta - db URL rewire, Lehrer switch offline not visible when not needed, Scrollbar invisible, anti-tracking improvements, gtag 'version', Desktop PWA support added, killswitch added

--- source/Backend/mirror.php
+++ source/Backend/mirror.php
@@ -1,4 +1,5 @@
 <?php
+
 if($_GET['hash'] == 'true'){
     echo hash_file("sha256", "mirror.php");
     exit;
@@ -35,10 +36,10 @@ if ($_GET && $_GET['url']) {
     $headers = getallheaders();
     $headers_str = [];
     $url = $_GET['url'];
-	if(str_pos($url, 'http://gymnasium-wuerselen.de/untis/' == false){
-		http_response_code(400);
-		exit;
-	}
+    if (false === strpos($url, 'http://gymnasium-wuerselen.de/untis/')) {
+      http_response_code(400);
+      exit;
+    }
 
     foreach ( $headers as $key => $value){
         if($key == 'Host' or $key == 'Cookie')
