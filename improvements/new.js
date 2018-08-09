2e8fb0b5752b9d719d6bd00799e370d19bc38407
FoseFx <fosefx@pm.me>
2018-08-09 14:21:07 +0200

    New Algo now produces the same Output as the old one

--- improvements/new.js
+++ improvements/new.js
@@ -2,17 +2,12 @@ function newF(ret, lehrer) {
     lehrer = lehrer || false;
     // .toLowerCase
     const SACONDITION = ['selbstständiges arbeiten', "selbständiges arbeiten", "selbstständies arbeiten", "selbstäniges arbeiten"];
-    const VARTEN =      ['entfall', 'vertretung', 'statt-vertretung', 'raum-vtr.', 'klausur'];
-    const VABKUERZUNG = ['e',       'v',            'statt-v',          'r',        'k'];
+    const VARTEN =      ['entfall', 'vertretung', 'statt-vertretung', 'raum-vtr.', 'klausur', "absenz"];
+    const VABKUERZUNG = ['e',       'v',            'statt-v',          'r',        'k',       'fehlt'];
     let stufen = [];
-    function addNewStufe(stufe) {
-        stufen.push({
-            stufe: stufe,
-            cntnd: []
-        });
-    }
+
     function typeAbkuerzen(type, infotext) {
-        let sa = infotext.includes('Selbst. Arb.');
+        let sa = infotext.includes('SELBST. ARB.');
         let index = VARTEN.findIndex(art=>art===type);
         if(index === -1 && !sa) return type;
         return sa? 'e (v)' : VABKUERZUNG[index];
@@ -21,26 +16,27 @@ function newF(ret, lehrer) {
     let returnArray = [];
     const parser = new DOMParser();
     const doc = parser.parseFromString(ret, "text/html");
+
     // return next file
     returnArray[0] = doc.querySelector('meta[http-equiv="refresh"]').content.split('URL=')[1];
 
-    doc.querySelectorAll('tr.list.odd,tr.list.even').forEach(function (zeile, zeileI, zeileArr) {
+    doc.querySelectorAll('tr.list.odd,tr.list.even').forEach(function (zeile) {
         if(zeile.children.length === 0) throw new Error("Zeile has no children");
         let children = zeile.children;
 
         const firstChild = zeile.firstChild;
+
         // 'inline_header' is the classname of the td that indicates a new Stufe
         if(/( |^)inline_header( |$)/.test(firstChild.className)) {
-            addNewStufe(firstChild.innerText.split(' ')[0]);
+            stufen.push({
+                stufe: firstChild.innerText.split(' ')[0],
+                cntnd: []
+            });
             return;
         }
 
-        //
-        // Information
-        //
         let date = "";
-        let oldroom;
-        let lehrer;
+        let oldroom = !lehrer? children[4].innerText.replace(/\s/g, ""): '?';
         let klasse;
         let stunden = children[1].innerText.replace(/\s/g, "");
         stunden = stunden.split("-"); // [1, 2]
@@ -50,27 +46,20 @@ function newF(ret, lehrer) {
         let infotext = children[lehrer?7:6].innerText.replace('\u00a0', '');
 
         SACONDITION.forEach(cond => {
-            infotext = infotext.toUpperCase().replace(cond.toUpperCase(), 'Selbst. Arb.');
+            infotext = infotext.toUpperCase().replace(cond.toUpperCase(), 'SELBST. ARB.');
         });
         infotext = infotext.replace('AUFGABEN', 'AUFG.');
         type = typeAbkuerzen(type, infotext);
 
         if (!lehrer){
             date = children[0].innerText.replace(/\s/g, "");
-            oldroom = children[4].innerText.replace(/\s/g, "");
         }else{
             doc.getElementsByClassName('mon_title')[0].innerText.split('.').forEach((value, index, array) => {
                 if(index === array.length -1) return;
                 date += value.replace(" ", "") + ".";
             });
-            lehrer = children[0].innerText.replace(/\s/g, "");
             klasse = children[2].innerText.replace(/\s/g, "");
         }
-
-        //
-        // Compile Infos
-        //
-
         let pushObj = {
             type: type,
             date: date,
@@ -83,17 +72,18 @@ function newF(ret, lehrer) {
             pushObj.stufe = klasse;
         }
         stunden.forEach((stunde, i, array) => {
-            pushObj.stunde = stunde;
-            stufen[stufen.length - 1].cntnd.push(pushObj);
+            let obj = Object.assign({}, pushObj);
+            obj.stunde = stunde;
+            stufen[stufen.length - 1].cntnd.push(obj);
 
             if (i === (array.length - 1)) {
                 let alls = "";
-                array.forEach((ts) => {
-                    alls += ts + " - "
+                array.forEach((ts, ind) => {
+                    alls += ts + (ind<array.length-1?" - ":"");
                 });
-                alls = alls.substr(0, alls.length - 3);
-
-                let obj = Object.assign({stunde: alls, nd: 1}, pushObj);
+                obj = Object.assign({}, pushObj);
+                obj.stunde = alls;
+                obj.nd = 1;
                 stufen[stufen.length - 1].cntnd.push(obj);
             }
         });
