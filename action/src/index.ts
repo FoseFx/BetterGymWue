59617237168997b75ca1904b18fb39441dd6561d
FoseFx <fosefx@pm.me>
2018-09-21 18:42:45 +0200

    Google Assistent? Lets to this

---
+++ action/src/index.ts
@@ -0,0 +1,17 @@
+import * as http from "https";
+
+import {dialogflow, SimpleResponse} from "actions-on-google";
+
+const app = dialogflow({debug: true});
+
+app.intent("Stundenplan", async function (conv) {
+    // const data = await getData();
+
+    conv.close(new SimpleResponse({
+        text: "Fresher test Text",
+        speech: "Fresher test yo"
+    }));
+});
+
+
+http.createServer(app).listen(45634, () => {console.log("Started Actions Server")});
