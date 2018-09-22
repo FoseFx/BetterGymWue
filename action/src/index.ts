64b396ea1ebf6961b33a3f931ec9cc010870fbb1
FoseFx <fosefx@pm.me>
2018-09-22 10:52:14 +0200

    s

--- action/src/index.ts
+++ action/src/index.ts
@@ -1,11 +1,12 @@
-import * as http from "https";
+import * as http from "http";
 
-import {dialogflow, SimpleResponse} from "actions-on-google";
+import {Conversation, dialogflow, SimpleResponse} from "actions-on-google";
 
 const app = dialogflow({debug: true});
 
-app.intent("Stundenplan", async function (conv) {
+app.intent("Stundenplan", function (conv: Conversation<any>) {
     // const data = await getData();
+
 
     conv.close(new SimpleResponse({
         text: "Fresher test Text",
