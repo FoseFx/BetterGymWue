bcf61c6c63ce3d4579c03bd57a330dce9eef4f9f
FoseFx <fosefx@pm.me>
2018-09-23 17:25:43 +0200

    fuck me, this will take time

--- action/src/intents/SignInIntent.ts
+++ action/src/intents/SignInIntent.ts
@@ -1,5 +1,9 @@
 import {Conversation, SignIn} from "actions-on-google";
 
 export function SignInIntent(conv: Conversation<any>, params, signin) {
-    return signin.status;
+    if (signin.status !== "ok")
+        conv.close("Ohne deine Erlaubnis kann ich nicht auf deine Kurse zugreifen.");
+    else
+        conv.close("Danke für dein Vertrauen! Ich beginne jetzt mit der Einrichtung.");
+    //TODO
 }
