d6317fdd54b59071c3172e4ef31a50335aaa78b6
FoseFx <fosefx@pm.me>
2018-09-23 11:11:20 +0200

    test2

--- action/src/index.ts
+++ action/src/index.ts
@@ -1,8 +1,12 @@
 import * as http from "http";
+import * as express from "express";
 
 import {Conversation, dialogflow, SimpleResponse} from "actions-on-google";
 
 const app = dialogflow({debug: true});
+const eapp = express();
+eapp.use(express.json());
+eapp.use(app);
 
 app.intent("Stundenplan", function (conv: Conversation<any>) {
     // const data = await getData();
@@ -14,5 +18,7 @@ app.intent("Stundenplan", function (conv: Conversation<any>) {
     }));
 });
 
+eapp.listen(45634, () => {console.log("Started Actions Server")});
 
-http.createServer(app).listen(45634, () => {console.log("Started Actions Server")});
+
+// http.createServer(app).listen(45634, () => {console.log("Started Actions Server")});
