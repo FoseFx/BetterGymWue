5cc28c2863cc996d8ea99d4a3cefeb6d3f8404ea
FoseFx <fosefx@gmail.com>
2018-10-15 12:58:02 +0200

    Added Unit Tests for Cache.ts

--- backend/src/app.spec.ts
+++ backend/src/app.spec.ts
@@ -20,19 +20,22 @@ describe('App', function () {
 
         // Note: this is a live test!
         it('should fail on invalid credentials', async function () {
+            const a0 = chai.request(app);
+            const a1 = chai.request(app);
+            const a2 = chai.request(app);
             const r = await Promise.all([
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/26/c/c00006.htm").auth("a", "b"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/26/c/c00006.htm").auth("ü", " ü\\$ß"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/26/c/c00006.htm").auth("", ""),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Schueler/f1/subst_001.htm").auth("a", "b"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Schueler/f1/subst_001.htm").auth("ü", "u\\$ß"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Schueler/f1/subst_001.htm").auth("", ""),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer/f1/subst_002.htm").auth("a", "b"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer/f1/subst_002.htm").auth("ü", "u\\$ß"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer/f1/subst_002.htm").auth("", ""),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer-Stundenplan/38/t/t00013.htm").auth("a", "b"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer-Stundenplan/38/t/t00013.htm").auth("ü", "u\\$ß"),
-                chai.request(app).get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer-Stundenplan/38/t/t00013.htm").auth("", ""),
+                a0.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/26/c/c00006.htm").auth("a", "b"),
+                a0.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/26/c/c00006.htm").auth("ü", " ü\\$ß"),
+                a0.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/26/c/c00006.htm").auth("", ""),
+                a0.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler/f1/subst_001.htm").auth("a", "b"),
+                a1.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler/f1/subst_001.htm").auth("ü", "u\\$ß"),
+                a1.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler/f1/subst_001.htm").auth("", ""),
+                a1.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer/f1/subst_002.htm").auth("a", "b"),
+                a2.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer/f1/subst_002.htm").auth("ü", "u\\$ß"),
+                a2.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer/f1/subst_002.htm").auth("", ""),
+                a2.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer-Stundenplan/38/t/t00013.htm").auth("a", "b"),
+                a2.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer-Stundenplan/38/t/t00013.htm").auth("ü", "u\\$ß"),
+                a2.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer-Stundenplan/38/t/t00013.htm").auth("", ""),
             ]);
 
             r.forEach((res:any, i:number)=>{
@@ -83,7 +86,27 @@ describe('App', function () {
     // Note: This is a live test!
     describe('Correct requests', function () {
 
+        it('should answer with content requested', async function () {
+            const a = chai.request(app);
+            const r = await Promise.all([
+                a.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler-Stundenplan/26/c/c00006.htm").set("Authorization",process.env.TEST_SCH_CREDS),
+                a.get("/v2/http://gymnasium-wuerselen.de/untis/Schueler/f1/subst_001.htm").set("Authorization",process.env.TEST_SCH_CREDS),
+                a.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer/f1/subst_001.htm").set("Authorization",process.env.TEST_LEHRER_CREDS),
+                a.get("/v2/http://gymnasium-wuerselen.de/untis/Lehrer-Stundenplan/38/t/t00001.htm").set("Authorization",process.env.TEST_LEHRER_CREDS),
+            ]);
 
+            const expects: RegExp[] = [
+                /^.*06B.*Gymnasium der Stadt W.rselen.*$/,
+                /^.*Datum.*Stunde.*\(Fach\).*Art.*\(Raum\).*Raum.*Vertretungs-Text.*Gymnasium der Stadt W.rselen.*$/,
+                /^.*Vertr\..*Std.*Klasse.*Fach.*Raum.*fehlt.*Art.*Vertretungs-Text.*Gymnasium der Stadt W.rselen.*$/,
+                /^.*Ad.*Gymnasium der Stadt W.rselen.*$/,
+            ];
+
+            r.forEach(function (res, i) {
+                expect(res.text.replace(/(\n|\r|\n\r)/g, "")).to.match(expects[i]);
+            });
+
+        });
 
     });
 
