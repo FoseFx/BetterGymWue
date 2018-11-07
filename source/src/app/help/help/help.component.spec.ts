d6ad8c910fa010c20a15ec4f223ecae26a4fe562
FoseFx <fosefx@pm.me>
2018-11-07 20:52:10 +0100

    1.5.3 Beta - Sortierung aufgrund von Problemen entfernt, aufnahme von 'Eigenständigem Arbeiten'

--- source/src/app/help/help/help.component.spec.ts
+++ source/src/app/help/help/help.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelpComponent } from './help.component';
+import {FormsModule} from "@angular/forms";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSlideToggleModule
+} from "@angular/material";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import {CommonModule} from "@angular/common";
+import {HelpRoutingModule} from "../help-routing.module";
 
 describe('HelpComponent', () => {
   let component: HelpComponent;
@@ -8,6 +20,17 @@ describe('HelpComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        HelpRoutingModule,
+        FormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatSlideToggleModule,
+        MatCardModule,
+        HttpClientModule
+      ],
       declarations: [ HelpComponent ]
     })
     .compileComponents();
--- source/src/app/main/s/base/base.service.ts
+++ source/src/app/main/s/base/base.service.ts
@@ -10,7 +10,7 @@ import {Kurs, TT} from "../../../Classes";
 
 @Injectable()
 export class BaseService {
-  public VERSION = "1.5.2 Beta";
+  public VERSION = "1.5.3 Beta";
   public acceptedAGB: boolean;
   allowedBrowser: boolean;
   public credentials: {u: string, p: string, l?: {u: string, p: string}};
