d358ece45d749a8db81a9b20e0313154fd085e5d
FoseFx <fosefx@pm.me>
2019-01-17 21:58:32 +0100

    1.6.1 Beta

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     next();
 });
 app.options("**/**", (_, res) => { res.end(); });
-app.get("/v2/version", (req, res) => res.json({ version: "1.6.0 Beta", news: ["AGB Änderungen", "Keine Analytics mehr", "Keine GithubAPI mehr"] }).end());
+app.get("/v2/version", (req, res) => res.json({ version: "1.6.1 Beta", news: ["'EVA' wird jetzt als 'E (V) angezeigt'", "Backend wird jetzt auf Heroku gehosted"] }).end());
 app.use(function (req, res, next) {
     const auth = req.headers.authorization;
     if (!auth)
