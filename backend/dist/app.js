1983d4bc8c47cbcfaf06437f96cc670deaf991eb
FoseFx <fosefx@pm.me>
2019-02-22 16:01:44 +0100

    testing CD

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
     next();
 });
 app.options("**/**", (_, res) => { res.end(); });
+app.get("/test", (req, res) => res.end("es ist 16 Uhr"));
 app.get("/v2/version", (req, res) => res.json({ version: "1.6.3 Beta", news: ["Production build", "Backend fixes, sorry für die Downtime", "Kurscloud reset", "Schulplaner Infos eingetragen"] }).end());
 app.use(function (req, res, next) {
     const auth = req.headers.authorization;
