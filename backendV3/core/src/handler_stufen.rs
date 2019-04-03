f2c7d52f4daea870a0933f79a077710349e8a66b
FoseFx <fosefx@pm.me>
2019-04-03 10:47:26 +0200

    Fixed Verified Guard

--- backendV3/core/src/handler_stufen.rs
+++ backendV3/core/src/handler_stufen.rs
@@ -1,13 +1,15 @@
 use crate::cache::redis::RedisConnection;
 use std::ops::Deref;
-use crate::fetch_data;
+use crate::{fetch_data, JwtSecret};
 use regex::Regex;
-use rocket::http::Status;
+use rocket::http::{Status, Cookie};
 use rocket::response::status::Custom;
 use rocket::request::FromRequest;
+use rocket::{request, State};
 use rocket::{Outcome, Request};
 use rocket::outcome::IntoOutcome;
 use frank_jwt::{decode, Algorithm};
+use rocket::outcome::Outcome::{Success, Failure};
 
 
 #[get("/stufen")]
@@ -25,18 +27,26 @@ pub fn get_stufen(connection: RedisConnection, verified: Verified) -> Custom<Str
 }
 
 
-struct Verified {
-
-}
+pub struct Verified(String);
 
 impl<'a, 'r> FromRequest<'a, 'r> for Verified {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> Outcome<Self, (Status, Self::Error), ()> {
-        return request.cookies()
-            .get("token")
-            .and_then(|cookie| cookie.value().parse().ok())
-            .and_then(|jwt| decode(jwt, JWT_SECRET, Algorithm::HS256).ok())
-            .or_forward(());
+        let secret = &request.guard::<State<JwtSecret>>().unwrap().0;
+
+        let mut cookies = request.cookies();
+        let token_res = cookies.get("token");
+        if token_res.is_some() {
+            let content = token_res.unwrap().value().to_string();
+            let jwt_res = decode(&content, secret, Algorithm::HS256);
+            if jwt_res.is_ok() {
+                let token: serde_json::Value = jwt_res.unwrap().1;
+                let creds = token.get("creds").unwrap().as_str().unwrap().to_string();
+                return Success(Verified(creds));
+            }
+            cookies.remove(Cookie::named("token"));
+        }
+        return Failure((Status::Unauthorized, ()));
     }
 }
