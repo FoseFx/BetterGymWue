1d7b73cb9f2b3a996355d9cb2df8dacbf2f343c9
FoseFx <fosefx@pm.me>
2019-05-22 18:53:23 +0200

    Added homework module

---
+++ source/src/app/homework/homework.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeworkRoutingModule } from './homework-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeworkRoutingModule
+  ]
+})
+export class HomeworkModule { }
